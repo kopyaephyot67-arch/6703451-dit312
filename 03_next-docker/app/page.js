67083b95ea8238b8bdb9256ad7c5f77c31581a00
Pyae Phyo <kopyaephyo@rsu.ac.th>
2025-09-22 10:16:31 +0700

    first page

---
+++ 03_next-docker/app/page.js
@@ -0,0 +1,9 @@
+import Attractions from './components/Attractions';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <Attractions />
+    </main>
+  );
+}
