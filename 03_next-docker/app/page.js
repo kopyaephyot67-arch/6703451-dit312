9a4bd040bcf515b71c0c26748835b6298024e14e
Pyae Phyo <kopyaephyo.t67@rsu.ac.th>
2025-09-22 11:35:01 +0700

    first commit

--- 03_next-docker/app/page.js
+++
@@ -1,9 +0,0 @@
-import Attractions from './components/Attractions';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50">
-      <Attractions />
-    </main>
-  );
-}
